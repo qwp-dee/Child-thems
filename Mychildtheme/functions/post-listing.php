4ae7c9239c047c3fa87d93f438d835486ea012ec
WP-Developer <55971399+wp-deepak@users.noreply.github.com>
2023-07-01 13:27:32 +0530

    Update post-listing.php
    changes

--- Mychildtheme/functions/post-listing.php
+++ Mychildtheme/functions/post-listing.php
@@ -1,25 +1,25 @@
 <?php
 
 
-function misha_loadmore_ajax_handler(){
+function loadmore_ajax_handler(){
  
   // prepare our arguments for the query
   $args = json_decode( stripslashes( $_POST['query'] ), true );
   $args['paged'] = $_POST['page'] + 1; // we need next page to be loaded
   $args['post_status'] = 'publish';
  
-  // it is always better to use WP_Query but not here
+  //It is always better to use WP_Query but not here
   query_posts( $args );
  
   if( have_posts() ) :
  
-    // run the loop
+    //Run the loop
     while( have_posts() ): the_post();
  
-      // look into your theme code how the posts are inserted, but you can use your own HTML of course
+      //Look into your theme code and how the posts are inserted, but you can use your own HTML of course
       // do you remember? - my example is adapted for Twenty Seventeen theme
       get_template_part( 'template-parts/post/content', get_post_format() );
-      // for the test purposes comment the line above and uncomment the below one
+      //For the test purposes comment on the line above and uncomment the below one
       // the_title();
  
  
@@ -31,7 +31,7 @@ function misha_loadmore_ajax_handler(){
  
  
  
-add_action('wp_ajax_loadmore', 'misha_loadmore_ajax_handler'); // wp_ajax_{action}
-add_action('wp_ajax_nopriv_loadmore', 'misha_loadmore_ajax_handler'); // wp_ajax_nopriv_{action}
+add_action('wp_ajax_loadmore', 'loadmore_ajax_handler'); // wp_ajax_{action}
+add_action('wp_ajax_nopriv_loadmore', 'loadmore_ajax_handler'); // wp_ajax_nopriv_{action}
 
 
